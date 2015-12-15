123b042d003423fdd0e783402bf15196a79a7d0f
André Duarte <mail@andreduarte.net>
2015-12-15 05:21:08 +0000

    Log

--- api/cron/push.js
+++ api/cron/push.js
@@ -6,7 +6,7 @@ var quote = require('../api/services/Quote'),
 var sendPush = function (urlPath, message) {
   //urlPath = url.parse(urlPath);
 
-  wns.sendToastText01(urlPath, message, {
+  wns.sendToastText02(urlPath, message, 'teste', {
     client_id: 'ms-app://S-1-15-2-1103054980-4181359820-1462783700-1197856228-4263398619-2624448765-1742964821',
     client_secret: '3fbpyX6fct5BKy6E7aDUhO3Vkj7lKnzp'
   }, function (error, result) {
