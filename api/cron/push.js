a7aed4ee5d1fe344a96c41bc9869070118c2f802
André Duarte <mail@andreduarte.net>
2015-12-15 05:26:58 +0000

    Log

--- api/cron/push.js
+++ api/cron/push.js
@@ -3,10 +3,10 @@ var quote = require('../api/services/Quote'),
   http = require('http'),
   url = require('url');
 
-var sendPush = function (urlPath, message) {
+var sendPush = function (urlPath, message, message2) {
   //urlPath = url.parse(urlPath);
 
-  wns.sendToastText02(urlPath, message, 'teste', {
+  wns.sendToastText02(urlPath, message, message2, {
     client_id: 'ms-app://S-1-15-2-1103054980-4181359820-1462783700-1197856228-4263398619-2624448765-1742964821',
     client_secret: '3fbpyX6fct5BKy6E7aDUhO3Vkj7lKnzp'
   }, function (error, result) {
@@ -31,12 +31,12 @@ module.exports = {
               result = _.last(result);
               if (stock.lowerLimit && stock.lowerLimit >= result) {
                 console.log('LOWER');
-                return sendPush(stock.user.uid, stock.name + ' upper limit hit!');
+                return sendPush(stock.user.uid, stock.name + ' upper limit hit!', 'Value: ' + result);
               }
 
               if (stock.upperLimit && stock.upperLimit <= result) {
                 console.log('UPPER');
-                return sendPush(stock.user.uid, stock.name + ' lower limit hit!');
+                return sendPush(stock.user.uid, stock.name + ' lower limit hit!', 'Value: ' + result);
               }
             });
         });
